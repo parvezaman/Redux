100675b0d8a06605317b94d5194e09a21b3d6f6f
parvezaman <emailtoamanullah@gmail.com>
2023-01-24 18:41:02 +0600

    understand immutability

---
+++ src/concepts/immutability.js
@@ -0,0 +1,28 @@
+let name = ['aman', 'ullah'];
+console.log(name);
+
+/*
+ output: [
+    "aman",
+    "ullah"
+]
+*/
+
+name = ['parvez']
+console.log(name);
+
+/*
+ output: [
+    "parvez"
+]
+]
+*/
+
+name = [...name, 'parvez']
+console.log(name); 
+
+/* output: [
+    "aman",
+    "ullah",
+    "parvez"
+] */
